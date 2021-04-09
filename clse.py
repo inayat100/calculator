5d93adf3702f8f2c921e16499c482b7296cc3fa6
inayat100 <68042481+inayat100@users.noreply.github.com>
2021-04-09 18:31:27 +0530

    Add files via upload

---
+++ clse.py
@@ -0,0 +1,57 @@
+def two_no(fn,sn,op):
+    if op=='+':
+        return   fn+sn
+    elif op=='-':
+        return fn - sn
+    elif op=='*':
+        return fn * sn
+    elif op=='/':
+        return fn / sn
+    else:
+        print("invalid input")
+def old_no(old,new,opt):
+    if opt=='+':
+        return  old + new
+    elif opt=='-':
+        return old - new
+    elif opt=='*':
+        return old * new
+    elif opt=='/':
+        return old/ new
+    else:
+        print("invalid input")
+while True:
+    while True:
+        print("enter first no")
+        fn = int(input())
+        print("enter second no")
+        sn = int(input())
+        print("enter operator")
+        op = input()
+        old_ans=two_no(fn,sn,op)
+        print("----------------    current value= ",old_ans,"      -------------------------")
+        print("Do you want to take old Ans... preass Y/N\n")
+        ag=input()
+        if ag=='y' or ag=='Y':
+            break
+    while (ag=='y' or ag=='Y'):
+        print("------------------     old_value= ", old_ans, " ---------------------")
+        print("enter the valuye ")
+        new=int(input())
+        print("enter the operator")
+        opt=input()
+        ol=old_no(old_ans,new,opt)
+        print(">>>>>>>>>>>>>>>>>>   current value= ", ol, "<<<<<<<<<<<<<<<<<<<<<<")
+        # print(ol)
+        old_ans=ol
+        print("Do you want to take old Ans... preass Y/N\n")
+        agn=input()
+        if agn=='y' or agn=='Y':
+            continue
+        else:
+            break
+
+
+
+
+
